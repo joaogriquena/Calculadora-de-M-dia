a5b3baf4b379859f3468a40f11052bc9c72e8cf7
Joao Guilherme <jgriquena@gmail.com>
2024-11-03 19:52:45 -0300

    .

--- main.js
+++ main.js
@@ -1,13 +1,25 @@
 const form = document.getElementById('form-atividade')
 const imgAprovado = '<img src=" ./images/aprovado.png" alt="Emoji celebrando" />'
 const imgReprovado = '<img src=" ./images/reprovado.png" alt="Emoji Triste" />'
+const atividades = []
+const notas = []
+
 let linhas = ''
+
 form.addEventListener('submit', function (e) {
     e.preventDefault()
 
-    const inputNomeAtividade = document.getElementById('nome-atividade')
-    const inputNotaAtividade = document.getElementById('nota-atividade')
+    adicionaLinha()
+    atualizaTabela()
+    atualizaMediaFinal()
+})
 
+function adicionaLinha() {
+    let inputNomeAtividade = document.getElementById('nome-atividade')
+    let inputNotaAtividade = document.getElementById('nota-atividade')
+
+    atividades.push(inputNomeAtividade.value)
+    notas.push(parseFloat(inputNotaAtividade.value))
 
     let linha = "<tr>"
     linha += `<td>${inputNomeAtividade.value}</td>`
@@ -17,9 +29,26 @@ form.addEventListener('submit', function (e) {
 
     linhas += linha
 
-    const corpoTabela = document.querySelector('tbody')
-    corpoTabela.innerHTML = linhas
-
     inputNomeAtividade = ''
     inputNotaAtividade = ''
-})
+}
+
+function atualizaTabela() {
+    const corpoTabela = document.querySelector('tbody')
+    corpoTabela.innerHTML = linhas
+}
+
+function atualizaMediaFinal() {
+    let mediaFinal = calculaMediaFinal()
+}
+
+function calculaMediaFinal() {
+    let somaDasNotas = 0
+
+    for (let i = 0; i < notas.length; i++) {
+        somaDasNotas += notas[i]
+    }
+
+    return somaDasNotas / notas.length
+
+}
