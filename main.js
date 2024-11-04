2bc7ce77a2352723fed68dd9f3cdfb00cb1ad4ac
Joao Guilherme <jgriquena@gmail.com>
2024-11-04 12:22:30 -0300

    .

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ const imgAprovado = '<img src=" ./images/aprovado.png" alt="Emoji celebrando" />
 const imgReprovado = '<img src=" ./images/reprovado.png" alt="Emoji Triste" />'
 const atividades = []
 const notas = []
+const spanAprovado = '<span class="resultado aprovado">Aprovado</span>'
+const spanReprovado = '<span class="resultado reprovado">Reprovado</span>'
+const notaMinima = parseFloat(prompt('Digite a nota minima:'))
 
 let linhas = ''
 
@@ -18,13 +21,18 @@ function adicionaLinha() {
     let inputNomeAtividade = document.getElementById('nome-atividade')
     let inputNotaAtividade = document.getElementById('nota-atividade')
 
+    if (atividades.includes(inputNomeAtividade.value)) {
+        alert(`Atividade ${inputNomeAtividade.value} já foi cadastrada`)
+        return;
+    }
+
     atividades.push(inputNomeAtividade.value)
     notas.push(parseFloat(inputNotaAtividade.value))
 
     let linha = "<tr>"
-    linha += `<td>${inputNomeAtividade.value}</td>`
-    linha += `<td>${inputNotaAtividade.value}</td>`
-    linha += `<td>${inputNotaAtividade.value >= 7 ? imgAprovado : imgReprovado}</td>`
+    linha += `<td> ${inputNomeAtividade.value}</td> `
+    linha += `<td> ${inputNotaAtividade.value}</td> `
+    linha += `<td> ${inputNotaAtividade.value >= notaMinima ? imgAprovado : imgReprovado}</td> `
     linha += "</tr>"
 
     linhas += linha
@@ -40,6 +48,9 @@ function atualizaTabela() {
 
 function atualizaMediaFinal() {
     let mediaFinal = calculaMediaFinal()
+
+    document.getElementById('media-final-valor').innerHTML = mediaFinal
+    document.getElementById('media-final-resultado').innerHTML = mediaFinal >= notaMinima ? spanAprovado : spanReprovado
 }
 
 function calculaMediaFinal() {
